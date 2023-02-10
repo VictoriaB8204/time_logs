4f604035ce82c13225cf9a48d865f728577137d1
VictoriaB8204 <vichka-lapochka.ru@mail.ru>
2023-02-10 18:19:04 +0600

    changed importing resources

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,5 +8,5 @@
 @endsection
 
 @section('scripts')
-    <script src="{{ mix('/public/js/users/index.js') }}" defer></script>
+    <script src="{{ mix('js/users/index.js') }}" defer></script>
 @endsection
