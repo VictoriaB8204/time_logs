3004987d13677df0c4751f08699c77e142d5cf0c
VictoriaB8204 <vichka-lapochka.ru@mail.ru>
2023-02-12 20:50:22 +0600

    передвинула кнопку создания лога

--- resources/views/time_logs/index.blade.php
+++ resources/views/time_logs/index.blade.php
@@ -1,9 +1,9 @@
 <div id="page_content">
 {{--    @include('time_logs.create')--}}
 
-    <div class="d-flex justify-content-between">
-        <h2>Current week</h2>
-        <div class="m-1">
+    <div class="d-flex">
+        <h2 class="me-5">Current week</h2>
+        <div>
             <button type="button" id="create_button" class="btn btn-primary btn-sm" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add time log">
                 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
