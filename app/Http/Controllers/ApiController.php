6ecf9635ee3d154b52afb3774103282086f9676e
VictoriaB8204 <vichka-lapochka.ru@mail.ru>
2023-02-14 00:09:24 +0600

    added api for current week logs

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 
 class ApiController extends Controller
 {
     public function getCurrentWeekTimelogs($email){
-        $user = User::where('email', $email)->first();
-        return $user->time_logs_for_payment
-            ->pluck('id')
-            ->toJson();
+//        $user = User::where('email', $email)->first();
+//        return $user->time_logs_for_payment
+//            ->pluck('id')
+//            ->toJson();
+        return Carbon::now();
     }
 }
